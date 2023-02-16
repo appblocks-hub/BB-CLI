7502c850e2a3402034d0a9a32978e52431f62ef0
devthalal <thalal.hassan@neoito.com>
2023-02-16 13:24:53 +0530

    feat: function handler accept event as param instead of req,res

--- templates/createTemplates/function-templates/generate-index.js
+++ templates/createTemplates/function-templates/generate-index.js
@@ -1,7 +1,9 @@
 const generateIndex = (funcName) => `
 
-const handler = async (req, res) => {
+const handler = async (event) => {
 
+  const {req, res} = event
+  
   // health check
   if (req.params["health"] === "health") {
     res.write(JSON.stringify({success: true, msg: "Health check success"}))
