2b38d058e832d7b73c6af18178b305d2b85d4225
Digambaran <arjunsreekumar.1@gmail.com>
2023-03-22 10:30:21 +0530

    feat:  husky for newly created templates
    huksy pre-commit, commist-msg hooks will be added by default on creation
    
    conventional commit for commit-msg

---
+++ templates/createTemplates/uiContainer-templates/generate-prettierrc.js
@@ -0,0 +1,12 @@
+const generatePrettierRc = () => `
+{
+    "trailingComma": "es5",
+    "tabWidth": 2,
+    "semi": false,
+    "printWidth": 120,
+    "singleQuote": true,
+    "arrowParens": "always"
+  } 
+ `
+
+module.exports = { generateUiContainerPrettierRc: generatePrettierRc }
