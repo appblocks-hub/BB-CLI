a9e19c4f7940be04c89f1ce783c6f685a1a5fcb8
Digambaran <arjunsreekumar.1@gmail.com>
2023-05-18 18:43:45 +0530

    refactor: removed unused packages

--- loader.js
+++ loader.js
@@ -1,9 +1,7 @@
-const ora = require('ora')
 const Spinnies = require('spinnies')
 
 const spinnies = new Spinnies({
   succeedColor: 'whiteBright',
   failColor: 'whiteBright',
 })
-const spinner = ora()
-module.exports = { spinner, spinnies }
+module.exports = { spinnies }
